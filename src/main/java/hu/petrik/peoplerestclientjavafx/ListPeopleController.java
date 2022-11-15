fc7cf0e06bb94ad118afcefb41659e3a7eb553a1
Sárándi <sarandi.balint@gmail.com>
2022-11-15 15:37:08 +0100

    gson setup

--- src/main/java/hu/petrik/peoplerestclientjavafx/ListPeopleController.java
+++ src/main/java/hu/petrik/peoplerestclientjavafx/ListPeopleController.java
@@ -1,11 +1,14 @@
 package hu.petrik.peoplerestclientjavafx;
 
+import com.google.gson.Gson;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.io.IOException;
 
 public class ListPeopleController {
 
@@ -19,6 +22,21 @@ public class ListPeopleController {
     private TableColumn<Person, String> emailCol;
     @FXML
     private TableColumn<Person, Integer> ageCol;
+
+    @FXML
+    private void initialize() {
+        try {
+            Response response = RequestHandler.get(App.BASE_URL);
+            String content = response.getContent();
+            Gson converter = new Gson();
+            Person[] people = converter.fromJson(content, Person[].class);
+            for(Person person :people){
+                peopleTable.getItems().add(person);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     public void insertClick(ActionEvent actionEvent) {
